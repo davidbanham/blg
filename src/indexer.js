da31b5fe3ce25b69273a740150bff9b5d38fbf19
David Banham <david@banham.id.au>
2015-04-05 14:34:32 +1000

    Add default width for images

--- src/indexer.js
+++ src/indexer.js
@@ -1,6 +1,11 @@
 var mustache = require('mustache');
 var template = `
 <html>
+<head>
+  <style>
+    img { width: 100% }
+  </style>
+</head>
 <body>
 {{#posts}}
   <h2><a href="{{path}}">{{title}}</a></h2>
